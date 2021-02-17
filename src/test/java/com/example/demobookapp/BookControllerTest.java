a2f2f537ea287479ad15594453a49e0d188cae72
Przemysław Baran <przemo@MacBook-Pro-Przemysaw.local>
2021-02-17 10:58:03 +0100

    First test failed

--- src/test/java/com/example/demobookapp/BookControllerTest.java
+++ src/test/java/com/example/demobookapp/BookControllerTest.java
@@ -1,15 +1,24 @@
 package com.example.demobookapp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(BookController.class)
@@ -24,14 +33,28 @@ public class BookControllerTest {
     @MockBean
     private BookService bookService;
 
+    @Captor
+    private ArgumentCaptor<BookRequest> argumentCaptor;
+
     @Test
-    public void postingANewBookShouldCreateANewBookInTheDatabase() {
+    public void postingANewBookShouldCreateANewBookInTheDatabase() throws Exception {
 
         BookRequest bookRequest = new BookRequest();
         bookRequest.setAuthor("Duke");
         bookRequest.setTitle("Java 11");
-        bookRequest.setIsbn("32423");
+        bookRequest.setIsbn("1337");
 
-        when(bookService.createNewBook()).thenReturn(1L);
+        when(bookService.createNewBook(argumentCaptor.capture())).thenReturn(1L);
+
+        this.mockMvc.perform(post("/api/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(bookRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", "http://localhost/api/books/1"));
+
+        assertThat(argumentCaptor.getValue().getAuthor(), is("Duke"));
+        assertThat(argumentCaptor.getValue().getIsbn(), is("1337"));
+        assertThat(argumentCaptor.getValue().getTitle(), is("Java 11"));
     }
 }
