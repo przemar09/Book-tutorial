33d0977cd12adb176d282fb2b3c16a8868347e4a
Przemysław Baran <przemo@MacBook-Pro-Przemysaw.local>
2021-02-17 11:09:23 +0100

    Posting new book test ok

--- src/main/java/com/example/demobookapp/BookController.java
+++ src/main/java/com/example/demobookapp/BookController.java
@@ -1,11 +1,35 @@
 package com.example.demobookapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 
+    @Autowired
     private BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Void> createNewBook(@Valid @RequestBody BookRequest bookRequest, UriComponentsBuilder uriComponentsBuilder) {
+
+        Long id = bookService.createNewBook(bookRequest);
+
+        UriComponents uriComponents = uriComponentsBuilder.path("/api/books/{id}").buildAndExpand(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponents.toUri());
+
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/example/demobookapp/BookService.java
+++ src/main/java/com/example/demobookapp/BookService.java
@@ -1,12 +1,23 @@
 package com.example.demobookapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
+    @Autowired
+    private  BookRepository bookRepository;
 
     public Long createNewBook(BookRequest bookRequest) {
-        return null;
+
+        Book book = new Book();
+        book.setAuthor(bookRequest.getAuthor());
+        book.setIsbn(bookRequest.getIsbn());
+        book.setTitle(bookRequest.getTitle());
+
+        book = bookRepository.save(book);
+
+        return book.getId();
     }
 }
